1e5b8ee4eb5e69fec253a38fb23b1a7ebf21ffd9
naohisa0616 <naohisa0616@icloud.com>
2021-01-02 10:17:14 +0900

    Initial Commit

---
+++ Soccer_memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soccer_memo
+//
+//  Created by 宮崎直久 on 2021/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
