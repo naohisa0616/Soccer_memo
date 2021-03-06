b981f50ec22e173580f4e592cc0424b65b006617
naohisa0616 <naohisa0616@icloud.com>
2021-03-06 10:46:15 +0900

    以下の機能を実装 ・no_imageの画像設定・表示 ・追加ボタン押下時の、アラートの表示 ・アラートにてテキストを入力し、tableviewに追加 ・選手画像の追加・削除

--- Soccer_memo/DetailViewController.swift
+++ Soccer_memo/DetailViewController.swift
@@ -7,23 +7,51 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class DetailViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate, UITableViewDelegate, UITableViewDataSource {
     
+    // アイテムの型
+    class Item {
+        var title : String
+        var done: Bool = false
+        
+        init(title: String) {
+            self.title = title
+        }
+    }
+    
+    //遷移元から名前を取得用の変数を定義
     var data: String?
+    // この配列に作ったアイテムを追加していく
+    var itemArray: [Item] = []
+    
+    //TableViewの紐付け
+    @IBOutlet weak var detailListView: UITableView!
+    //追加ボタン
+    @IBAction func addMatchInfo(_ sender: Any) {
+        var textField = UITextField()
+                let alert = UIAlertController(title: "アイテムを追加", message: "", preferredStyle: .alert)
+                let action = UIAlertAction(title: "リストに追加", style: .default) { (action) in
+                    let newItem: Item = Item(title: textField.text!)
+                    self.itemArray.append(newItem)
+                    self.detailListView.reloadData()
+                }
+                
+                alert.addTextField { (alertTextField) in
+                    alertTextField.placeholder = "NEWアイテム"
+                    textField = alertTextField
+                }
+                
+                alert.addAction(action)
+                present(alert, animated: true, completion: nil)
+    }
+    
+    //選手名の表示ラベル
     @IBOutlet weak var playerName: UILabel!
     
     @IBOutlet weak var imageView: UIImageView! {
         didSet {
             // デフォルトの画像を表示する
             imageView.image = UIImage(named: "no_image.png")
-        }
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        if let data = self.data {
-            self.playerName.text = data
         }
     }
     
@@ -45,6 +73,38 @@ class DetailViewController: UIViewController, UIImagePickerControllerDelegate & 
     @IBAction func deletePicture(_ sender: UIButton) {
         // アラート表示
         showAlert()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //テーブルビューのデリゲートを設定する。
+        self.detailListView.delegate = self
+        //テーブルビューのデータソースを設定する。
+        self.detailListView.dataSource = self
+        // メモ一覧で表示するセルを識別するIDの登録処理を追加。
+        detailListView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        if let data = self.data {
+            //ラベルに選手名を表示
+            self.playerName.text = data
+        }
+    }
+    
+    // セルの数を指定ーitemArrayの配列の数だけCellを表示します
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    // Cellの内容を決める
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //「DetailCell」を引っ張ってくる
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailCell", for: indexPath)
+        //Cell番号のitemArrayを変数Itemに代入
+        let item = itemArray[indexPath.row]
+        //ToDoCellにCell番号のItemArrayの中身を表示させるようにしている
+        cell.textLabel?.text = item.title
+        //チェックマークを表示する処理ーitemのdoneがtrueだったら表示falseだったら非表示
+        cell.accessoryType = item.done ? .checkmark : .none
+        return cell
     }
     
     // アラート表示
